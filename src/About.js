27e40dcea27e6a78ea3ec172a96d02542ee78830
Raghav-B <raghavbhardwaj1998@gmail.com>
2020-01-17 12:35:53 +0800

    initial commit

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React from "react"
+ 
+function About(props) {
+  return (
+    <div align="center">
+      <h1>About</h1>
+
+      <table>
+            <ul> HAHAH </ul>
+            <ul> HAHAhahhf </ul>
+        </table>
+    </div>
+  )
+}
+ 
+export default About;
