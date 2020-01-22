5895c19e00cf87434c38304be9d190e89fcd9daa
Raghav-B <raghavbhardwaj1998@gmail.com>
2020-01-23 01:33:10 +0800

    Add working page implementation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import {NavHashLink as Link} from 'react-router-hash-link';
 import About from "./AboutMe/About.js";
@@ -7,50 +7,125 @@ import Projects from "./Projects/Projects.js";
 import Leadership from "./Leadership/Leadership.js";
 import './App.css';
 
-function App() {  
-  return (
-    <BrowserRouter>
-     
-      <div className="App">
+var isClickScrolling = false;
+var scrollTimer = -1;
+
+class App extends Component {  
+  constructor() {
+    super();
+  }
+  
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+  
+  handleScroll() {
+    if (isClickScrolling == false) {
+      var sectionArr = document.getElementsByClassName("section");
+      for (var i = 0; i < sectionArr.length; i++) {
+        const top = sectionArr[i].getBoundingClientRect().top;
+        if (top >= 0 && top <= window.innerHeight/2) {
+          window.location.hash = sectionArr[i].id;
+
+          var curSection = sectionArr[i].id+"_anchor";
+          var buttonArr = document.getElementsByClassName("content");
+
+          for (var i = 0; i < buttonArr.length; i++) {
+            if (buttonArr[i].id == curSection) {
+              buttonArr[i].className = "content nav_bar_selected";
+            } else {
+              buttonArr[i].className = "content nav_bar_unselected active";
+            }
+          }  
+        }
+      }
+    } else {
+      if (scrollTimer != -1) {
+        clearTimeout(scrollTimer);
+      }
+
+      scrollTimer = setTimeout(function() {
+        isClickScrolling = false;
+      }, 150);
+    }
+  }
+
+  navHighlight(event) {
+    isClickScrolling = true;
+
+    var curSection = event.target.id;
+    var buttonArr = document.getElementsByClassName("content");
+
+    for (var i = 0; i < buttonArr.length; i++) {
+      if (buttonArr[i].id == curSection) {
+        buttonArr[i].className = "content nav_bar_selected";
+      } else {
+        buttonArr[i].className = "content nav_bar_unselected active";
+      }
+    }  
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
         
-        <div id="navigation_bar" className="nav_bar">
+        <div className="App">
+          
+          <div id="navigation_bar" className="nav_bar">
 
-          <Link id="content" onClick={() => test()} to="#aboutme">About</Link>
-          <Link id="content" to="#workexperience">Work Experience</Link>
-          <Link id="content" to="#projects">Projects</Link>
-          <Link id="content" to="#leadership">Leadership</Link>
-          <Link id="content" to="#contactme">Contact Me</Link>
-        </div>
+            <Link 
+              id="aboutme_anchor"
+              className="content nav_bar_unselected"
+              onClick={(event) => this.navHighlight(event)}
+              to="#aboutme">About
+            </Link>
+            <Link 
+              id="workexperience_anchor"
+              className="content nav_bar_unselected" 
+              onClick={(event) => this.navHighlight(event)} 
+              to="#workexperience">Work Experience
+            </Link>
+            <Link 
+              id="projects_anchor"
+              className="content nav_bar_unselected" 
+              onClick={(event) => this.navHighlight(event)} 
+              to="#projects">Projects
+            </Link>
+            <Link 
+              id="leadership_anchor"
+              className="content nav_bar_unselected" 
+              onClick={(event) => this.navHighlight(event)} 
+              to="#leadership">Leadership
+            </Link>
+            <Link 
+              id="contactme_anchor"
+              className="content nav_bar_unselected" 
+              onClick={() => this.navHighlight()} 
+              to="#contactme">Contact Me
+            </Link>
+          </div>
 
-        <div id="aboutme">
-          <About></About>
-        </div>
-        <div id="workexperience">
-          <WorkExperience></WorkExperience>
-        </div>
-        <div id="projects">
-          <Projects></Projects>
-        </div>
-        <div id="leadership">
-          <Leadership></Leadership>
-        </div>
+          <div id="aboutme" className="section">
+            <About></About>
+          </div>
+          <div id="workexperience" className="section">
+            <WorkExperience></WorkExperience>
+          </div>
+          <div id="projects" className="section">
+            <Projects></Projects>
+          </div>
+          <div id="leadership" className="section">
+            <Leadership></Leadership>
+          </div>
 
-      </div>
-    </BrowserRouter>
-  );
-
-}
-
-function test() {
-  var btns = document.getElementsByName("content")
-  alert(btns.length);
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("test");
-    current[0].className = current[0].className.replace(" test", "");
-    this.className += " test";
-    });
-}
+        </div>
+      </BrowserRouter>
+    );
+  }  
 }
 
 export default App;
