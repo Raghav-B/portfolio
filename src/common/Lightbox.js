30e9e048fe9acb05ac4a5c68db74a42144eb3b0f
Raghav-B <raghavbhardwaj1998@gmail.com>
2021-09-23 12:18:07 +0800

    Add back modal for projects

--- src/common/Lightbox.js
+++ src/common/Lightbox.js
@@ -1,99 +1,151 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { useSwipeable } from "react-swipeable";
+import ReactModal from "react-modal";
 import "./Lightbox.css";
 
-const Lightbox = ({ selectedImage, onNext, onPrev, onClose }) => {
-  const [showLeftArrow, setShowLeftArrow] = useState(true);
-  const [showRightArrow, setShowRightArrow] = useState(true);
+const Lightbox = ({ selectedImage, onNext, onPrev, onClose, showModal, setShowModal }) => {
 
-  const swipeHandlers = useSwipeable({
-    onSwipedLeft: () => onNext(),
-    onSwipedRight: () => onPrev(),
-    onSwipedDown: e => onClose(),
-    trackMouse: true,
-    trackTouch: true,
-    preventDefaultTouchmoveEvent: true
-  });
+    const handleKeyPress = useCallback(
+        e => {
+            switch (e.code) {
+                case "Escape":
+                    onClose();
+                    break;
+                case "ArrowRight":
+                    onNext();
+                    break;
+                case "ArrowLeft":
+                    onPrev();
+                    break;
+                default:
+                    break;
+            }
+        },
+        [onClose, onNext, onPrev]
+    );
 
-  const handleKeyPress = useCallback(
-    e => {
-      switch (e.code) {
-        case "Escape":
-          onClose();
-          break;
-        case "ArrowRight":
-          onNext();
-          break;
-        case "ArrowLeft":
-          onPrev();
-          break;
-        default:
-          break;
-      }
-    },
-    [onClose, onNext, onPrev]
-  );
+    useEffect(() => {
+        window.addEventListener("keyup", handleKeyPress);
+        return () => {
+            window.removeEventListener("keyup", handleKeyPress);
+        };
+    }, [handleKeyPress]);
 
-  useEffect(() => {
-    window.addEventListener("keyup", handleKeyPress);
-    return () => {
-      window.removeEventListener("keyup", handleKeyPress);
-    };
-  }, [handleKeyPress]);
-
-  return (
-    <>
-      <div className="lightbox-backdrop" onClick={onClose} />
-      <div className="lightbox-window">
-        <div
-          style={{
-            backgroundColor: "white",
-            padding: "1%",
-            width: "100%",
-            position: "relative"
-          }}
+    return (
+        <ReactModal
+            isOpen={showModal}
+            style={{
+                overlay: {
+                    zIndex: 10,
+                },
+                content: {
+                    maxWidth: "70%",
+                    maxHeight: "90%",
+                    // position: "absolute",
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    margin: "auto",
+                    borderRadius: 0,
+                    padding: "1%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flexDirection: "column",
+                }
+            }}
+            shouldCloseOnEsc={true}
+            onRequestClose={() => {
+                setShowModal(false);
+            }}
         >
-          <img
-            className="back-button"
-            src={require("../imgs/Icons/left-arrow.svg")}
-            alt="back-button-icon"
-            onClick={onClose}
-          />
-          {selectedImage.description}
-        </div>
+            <div
+                style={{
+                    backgroundColor: "white",
+                    padding: "1%",
+                    width: "100%",
+                    position: "relative"
+                }}
+            >
+                <img
+                    className="back-button"
+                    src={require("../imgs/Icons/left-arrow.svg")}
+                    alt="back-button-icon"
+                    onClick={onClose}
+                />
+            </div>
+            <div
+                style={{
+                    // backgroundColor: "blue",
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center"
+                }}
+            >
+                {
+                    (showModal) ?
+                        <>
 
-        <div {...swipeHandlers} className="lightbox-image-container">
-          <div
-            className="arrow-container left-arrow"
-            onClick={onPrev}
-          // onMouseEnter={() => setShowLeftArrow(true)}
-          // onMouseLeave={() => setShowLeftArrow(false)}
-          >
-            {showLeftArrow ? (
-              <img src={require("../imgs/Icons/chevron-left.svg")} alt="left arrow icon" />
-            ) : null}
-          </div>
 
-          <img
-            id="lightbox-image"
-            src={selectedImage.src}
-            alt={selectedImage.alt}
-          />
+                            <div
+                                className="arrow-container left-arrow"
+                                onClick={onPrev}>
+                                <img src={require("../imgs/Icons/chevron-left.svg")} alt="left arrow icon" />
+                            </div>
 
-          <div
-            className="arrow-container right-arrow"
-            onClick={onNext}
-          // onMouseEnter={() => setShowRightArrow(true)}
-          // onMouseLeave={() => setShowRightArrow(false)}
-          >
-            {showRightArrow ? (
-              <img src={require("../imgs/Icons/chevron-right.svg")} alt="right arrow icon" />
-            ) : null}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                            <div
+                                style={{
+                                    textAlign: "center",
+                                    fontSize: "25px",
+                                    fontWeight: "bold",
+                                    marginBottom: "1%"
+                                }}
+                            >
+                                {selectedImage.title}
+                            </div>
+
+                            <div
+                                style={{
+                                    textAlign: "center",
+                                    fontSize: "20px",
+                                    paddingLeft: "10%",
+                                    paddingRight: "10%",
+                                    marginBottom: "1%"
+                                }}
+                            >
+                                {selectedImage.description}
+                            </div>
+                            <img
+                                id="lightbox-image"
+                                src={selectedImage.src}
+                                alt={selectedImage.alt}
+                            />
+
+                            <div
+                                className="arrow-container right-arrow"
+                                onClick={onNext}
+                            >
+                                <img src={require("../imgs/Icons/chevron-right.svg")} alt="right arrow icon" />
+                            </div>
+                        </> : null
+                }
+
+            </div>
+        </ReactModal >
+
+        // <>
+        //     <div className="lightbox-backdrop" onClick={onClose} />
+        //     <div className="lightbox-window">
+        //         
+
+        //         <div className="lightbox-image-container">
+        //             
+        //     </div>
+        // </>
+    );
 };
 
 export default Lightbox;
